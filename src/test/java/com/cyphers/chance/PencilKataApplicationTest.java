c6a61f3b82ff9d5234c0b9dcaad842887fd5df16
chance <chance.a.cyphers@gmail.com>
2018-05-30 17:15:29 -0400

    Initial commit

---
+++ src/test/java/com/cyphers/chance/PencilKataApplicationTest.java
@@ -0,0 +1,16 @@
+package com.cyphers.chance;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PencilKataApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
